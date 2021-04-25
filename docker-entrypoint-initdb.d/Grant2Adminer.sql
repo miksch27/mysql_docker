583f2beb207818c83e67291d1a80df5b17350724
Christian M <27285353+miksch27@users.noreply.github.com>
2021-04-25 20:52:51 +0200

    U

--- docker-entrypoint-initdb.d/Grant2Adminer.sql
+++ docker-entrypoint-initdb.d/Grant2Adminer.sql
@@ -12,3 +12,90 @@ CREATE TABLE Sensor.Measurement (
   Value DECIMAL(9, 4) NOT NULL,
   PRIMARY KEY (Date, Sensor, Valuename)
 );
+COMMIT;
+INSERT INTO
+  Sensor
+VALUES
+  (1);
+INSERT INTO
+  Sensor
+VALUES
+  (2);
+INSERT INTO
+  Valuename
+VALUES
+  (1, 'Temperature');
+INSERT INTO
+  Valuename
+VALUES
+  (2, 'Humidity');
+INSERT INTO
+  Valuename
+VALUES
+  (3, 'Pressure');
+-- Sensor 1 hat alle 3 Parameter (Temp, Feuchte und Druck)
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 00:00:00', 1, 1, 20.0);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 01:00:00', 1, 1, 21.0);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 02:00:00', 1, 1, 20.5);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 03:00:00', 1, 1, 19.8);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 00:00:00', 1, 2, 59);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 01:00:00', 1, 2, 57);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 02:00:00', 1, 2, 58);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 03:00:00', 1, 2, 69);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 00:00:00', 1, 3, 987);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 01:00:00', 1, 3, 989);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 02:00:00', 1, 3, 992);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 03:00:00', 1, 3, 990);
+-- Sensor 2 hat nur Temp
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 00:00:00', 2, 1, 25.0);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 01:00:00', 2, 1, 26.0);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 02:00:00', 2, 1, 25.5);
+INSERT INTO
+  Measurement
+VALUES
+  ('2020-03-10 03:00:00', 2, 1, 24.8);
